cd397e299378fc223f89673c3741f1e954f03a1d
Rodrigo B. de Oliveira <rbo@acm.org>
2017-04-06 13:00:08 -0300

    Copy artifactory configuration gradle-script-kotlin

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,4 +1,7 @@
 import org.gradle.api.publish.maven.*
+import org.jfrog.gradle.plugin.artifactory.dsl.PublisherConfig
+import org.jfrog.gradle.plugin.artifactory.dsl.ResolverConfig
+import groovy.lang.GroovyObject
 
 buildscript {
 
@@ -17,6 +20,7 @@ version = "0.0.1-SNAPSHOT"
 
 plugins {
     `maven-publish`
+    id("com.jfrog.artifactory") version "4.1.1"
 }
 
 apply {
@@ -44,3 +48,30 @@ publishing {
     }
 }
 
+artifactory {
+    setContextUrl("https://repo.gradle.org/gradle")
+    publish(delegateClosureOf<PublisherConfig> {
+        repository(delegateClosureOf<GroovyObject> {
+            val targetRepoKey = "libs-${buildTagFor(project.version as String)}s-local"
+            setProperty("repoKey", targetRepoKey)
+            setProperty("username", project.findProperty("artifactory_user") ?: "nouser")
+            setProperty("password", project.findProperty("artifactory_password") ?: "nopass")
+            setProperty("maven", true)
+        })
+        defaults(delegateClosureOf<GroovyObject> {
+            invokeMethod("publications", "mavenJava")
+        })
+    })
+    resolve(delegateClosureOf<ResolverConfig> {
+        setProperty("repoKey", "repo")
+    })
+}
+
+fun buildTagFor(version: String): String =
+    when (version.substringAfterLast('-')) {
+        "SNAPSHOT" -> "snapshot"
+        in Regex("""M\d+[a-z]*$""") -> "milestone"
+        else -> "release"
+    }
+
+operator fun Regex.contains(s: String) = matches(s)
